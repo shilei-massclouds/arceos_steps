a5909de5ea247eac93e565cc3459bc6eb4a97cc3
Shi Lei <shi_lei@massclouds.com>
2023-08-12 01:08:29 +0000

    allocator: support tlsf

--- crates/allocator/src/simple.rs
+++ crates/allocator/src/simple.rs
@@ -4,48 +4,78 @@
 
 use core::alloc::Layout;
 use core::num::NonZeroUsize;
+use core::ptr::NonNull;
+use rlsf::Tlsf;
 
-use crate::{AllocResult, BaseAllocator, ByteAllocator};
+use crate::{AllocError, AllocResult, BaseAllocator, ByteAllocator};
 
-/// A byte-granularity memory allocator based on the [simple_system_allocator].
+/// A TLSF (Two-Level Segregated Fit) memory allocator.
 ///
-/// [simple_system_allocator]: https://docs.rs/simple_system_allocator/latest/simple_system_allocator/
-pub struct SimpleByteAllocator;
+/// It's just a wrapper structure of [`rlsf::Tlsf`], with `FLLEN` and `SLLEN`
+/// fixed to 28 and 32.
+pub struct SimpleByteAllocator {
+    inner: Tlsf<'static, u32, u32, 28, 32>, // max pool size: 32 * 2^28 = 8G
+    total_bytes: usize,
+    used_bytes: usize,
+}
 
 impl SimpleByteAllocator {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            inner: Tlsf::new(),
+            total_bytes: 0,
+            used_bytes: 0,
+        }
     }
 }
 
 impl BaseAllocator for SimpleByteAllocator {
-    fn init(&mut self, _start: usize, _size: usize) {
-        todo!();
+    fn init(&mut self, start: usize, size: usize) {
+        unsafe {
+            let pool = core::slice::from_raw_parts_mut(start as *mut u8, size);
+            self.inner
+                .insert_free_block_ptr(NonNull::new(pool).unwrap())
+                .unwrap();
+        }
+        self.total_bytes = size;
     }
 
-    fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
-        todo!();
+    fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
+        unsafe {
+            let pool = core::slice::from_raw_parts_mut(start as *mut u8, size);
+            self.inner
+                .insert_free_block_ptr(NonNull::new(pool).unwrap())
+                .ok_or(AllocError::InvalidParam)?;
+        }
+        self.total_bytes += size;
+        Ok(())
     }
 }
 
 impl ByteAllocator for SimpleByteAllocator {
-    fn alloc(&mut self, _layout: Layout) -> AllocResult<NonZeroUsize> {
-        todo!();
+    fn alloc(&mut self, layout: Layout) -> AllocResult<NonZeroUsize> {
+        let ptr = self.inner.allocate(layout).ok_or(AllocError::NoMemory)?;
+        self.used_bytes += layout.size();
+        Ok(ptr.addr())
     }
 
-    fn dealloc(&mut self, _pos: NonZeroUsize, _layout: Layout) {
-        todo!();
+    fn dealloc(&mut self, pos: NonZeroUsize, layout: Layout) {
+        unsafe {
+            self.inner
+                .deallocate(NonNull::new_unchecked(pos.get() as _), layout.align())
+        }
+        self.used_bytes -= layout.size();
     }
 
     fn total_bytes(&self) -> usize {
-        todo!();
+        self.total_bytes
     }
 
     fn used_bytes(&self) -> usize {
-        todo!();
+        self.used_bytes
     }
 
     fn available_bytes(&self) -> usize {
-        todo!();
+        self.total_bytes - self.used_bytes
     }
 }
